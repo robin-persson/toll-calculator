d0299d8c718be9f3b3d4d5878cb3bf906ea0db92
Robin Persson <robin.persson@powerfactors.com>
2024-11-08 12:34:17 +0100

    TollCalculatorTestsBase: Flatten namespace

--- source/TollCalculatorTests/TollCalculatorTestsBase.cs
+++ source/TollCalculatorTests/TollCalculatorTestsBase.cs
@@ -4,48 +4,47 @@ using Nager.Date.HolidayProviders;
 using Nager.Date.Models;
 using TollFeeCalculator;
 
-namespace TollCalculatorTests
+namespace TollCalculatorTests;
+
+public class TollCalculatorTestsBase
 {
-    public class TollCalculatorTestsBase
+    protected int? result;
+    protected TollCalculator? calculator;
+    private Mock<IHolidayProvider> holidayProviderMock = new Mock<IHolidayProvider>();
+
+    public void Initialize()
     {
-        protected int? result;
-        protected TollCalculator? calculator;
-        private Mock<IHolidayProvider> holidayProviderMock = new Mock<IHolidayProvider>();
+        holidayProviderMock
+            .Setup(provider => provider.GetHolidays(It.IsAny<int>()))
+            .Returns(new List<Holiday>());
+    }
 
-        public void Initialize()
+    protected void GivenTollCalculator()
+    {
+        calculator = new TollCalculator(holidayProviderMock.Object);
+    }
+
+    protected void ThenItReturnsAResult()
+    {
+        result.Should().NotBe(null);
+    }
+
+    protected void ThenResultIs(int expectedResult)
+    {
+        result.Should().Be(expectedResult);
+    }
+
+    public void GivenHolidays(IEnumerable<string> dates)
+    {
+        holidayProviderMock
+            .Setup(provider => provider.GetHolidays(It.IsAny<int>()))
+            .Returns(holidays());
+
+        IEnumerable<Holiday> holidays()
         {
-            holidayProviderMock
-                .Setup(provider => provider.GetHolidays(It.IsAny<int>()))
-                .Returns(new List<Holiday>());
-        }
-
-        protected void GivenTollCalculator()
-        {
-            calculator = new TollCalculator(holidayProviderMock.Object);
-        }
-
-        protected void ThenItReturnsAResult()
-        {
-            result.Should().NotBe(null);
-        }
-
-        protected void ThenResultIs(int expectedResult)
-        {
-            result.Should().Be(expectedResult);
-        }
-
-        public void GivenHolidays(IEnumerable<string> dates)
-        {
-            holidayProviderMock
-                .Setup(provider => provider.GetHolidays(It.IsAny<int>()))
-                .Returns(holidays());
-
-            IEnumerable<Holiday> holidays()
+            foreach (string date in dates)
             {
-                foreach (string date in dates)
-                {
-                    yield return new Holiday { Date = DateTime.Parse(date) };
-                }
+                yield return new Holiday { Date = DateTime.Parse(date) };
             }
         }
     }
