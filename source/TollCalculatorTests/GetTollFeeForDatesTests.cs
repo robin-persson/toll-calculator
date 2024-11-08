f476683df9efb19cf5f3a48685efb8401e520fdd
Robin Persson <robin.persson@powerfactors.com>
2024-11-08 10:36:18 +0100

    Add test Toll_DoesNotExceedDailyMaximum to support refactoring

--- source/TollCalculatorTests/GetTollFeeForDatesTests.cs
+++ source/TollCalculatorTests/GetTollFeeForDatesTests.cs
@@ -5,15 +5,28 @@ namespace TollCalculatorTests;
 public class GetTollFeeForDatesTests : TollCalculatorTestsBase
 {
     [Fact]
-    public void GetTollFee_IsCallable()
+    public void Toll_DoesNotExceedDailyMaximum()
     {
         GivenTollCalculator();
-        WhenGettingTollFeeWith(vehicle: Vehicle.Car, dates: [new DateTime(2021, 1, 1, 9, 0, 0)]);
-        ThenItReturnsAResult();
+        WhenGettingTollFeeWith(
+            vehicle: Vehicle.Car,
+            dates:
+            [
+                "2024-11-08 07:00:00",
+                "2024-11-08 08:00:00",
+                "2024-11-08 09:00:00",
+                "2024-11-08 10:00:00",
+                "2024-11-08 11:00:00",
+                "2024-11-08 12:00:00",
+                "2024-11-08 13:00:00",
+            ]
+        );
+        ThenResultIs(60);
+    }
 
-        void WhenGettingTollFeeWith(Vehicle vehicle, DateTime[] dates)
-        {
-            result = calculator!.GetTollFee(vehicle, dates);
-        }
+    private void WhenGettingTollFeeWith(Vehicle vehicle, string[] dates)
+    {
+        var dateTimes = from date in dates select DateTime.Parse(date);
+        result = calculator!.GetTollFee(vehicle, dateTimes.ToArray());
     }
 }
